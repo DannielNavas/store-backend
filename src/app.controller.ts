27038196b4e895281255b4fa653c612980511ddc
Danniel Navas <dannielnavas@gmail.com>
2023-03-16 06:08:02 -0500

    ✨ Query params

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -19,14 +19,36 @@ export class AppController {
     return 'con /sas/';
   }
   //TODO: se debe crear el enspoint con plurales tareas -> tasks personas -> people
-
+  // PARAMS
   @Get('products/:id')
-  getProducts(@Param('id') id: string) {
+  getProduct(@Param('id') id: string) {
     return `Product ${id}`;
   }
 
   @Get('categories/:id/products/:productId')
   getCategory(@Param('id') id: string, @Param('productId') productId: string) {
     return `Category ${id}, Product ${productId}`;
+  }
+
+  // QUERY PARAMS
+
+  // @Get('products')
+  // getProducts(@Query() params: any) {
+  //   const { limit, offset } = params;
+  //   return `Products: Limit => ${limit}, Offset => ${offset}`;
+  // }
+  // TODO: no importa el orden de los parametros
+  @Get('products')
+  getProducts(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('brand') brand: string,
+  ) {
+    return `Products: Limit => ${limit}, Offset => ${offset} Brand => ${brand}`;
+  }
+  // TODO: toma el filter como un parametro products/:id se organiza no dinamicas primeras y dinamicas despues
+  @Get('products/filter')
+  getProductFilter() {
+    return `Yo soy un filter`;
   }
 }
