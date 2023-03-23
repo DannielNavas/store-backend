59c533e267afb707184afd57bd0b8aaf20aba6ce
Danniel Navas <dannielnavas@gmail.com>
2023-03-23 06:14:38 -0500

    ✨ validar datos enviados

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import { AppModule } from './app.module';
 // TODO: ejecuta nuestra aplicacion
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  // TODO: va a quitar del payload todos los atributos que no esten definidos en el dto -with whitelist
+  // TODO con forbidNonWhitelisted se alerta en la respuesta de la api que se envia un atributo que no esta definido en el dto
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
