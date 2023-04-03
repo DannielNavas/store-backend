e403cd9225a6ba00d098df8fe50da4129c80c64b
Danniel Navas <dannielnavas@gmail.com>
2023-04-03 06:35:10 -0500

    ✨relaciones uno a muchos de forma envevida

--- src/users/entities/customer.entity.ts
+++ src/users/entities/customer.entity.ts
@@ -1,6 +1,19 @@
+import { Prop } from '@nestjs/mongoose';
+import { Types } from 'mongoose';
+
 export class Customer {
   id: number;
   name: string;
   lastName: string;
   phone: string;
+
+  @Prop({
+    type: [
+      {
+        name: { type: String, required: true },
+        color: { type: String, required: true },
+      },
+    ],
+  })
+  skills: Types.Array<Record<string, any>>;
 }
