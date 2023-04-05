c8ee6825d3af4a90f0a2cd4753eda2f396eb8ae3
Danniel Navas <dannielnavas@gmail.com>
2023-04-05 07:13:43 -0500

    ✨ basic jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from 'src/users/users.module';
+import { AuthController } from './controllers/auth/auth.controller';
 import { AuthService } from './services/auth/auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
-import { AuthController } from './controllers/auth/auth.controller';
 
 @Module({
   providers: [AuthService, LocalStrategy],
--- src/auth/services/auth/auth.service.ts
+++ src/auth/services/auth/auth.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+import { Token } from 'src/auth/models/token.model';
+import { User } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/services/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersServices: UsersService) {}
+  constructor(
+    private usersServices: UsersService,
+    private jwtServices: JwtService,
+  ) {}
 
   async validateUser(email: string, password: string) {
     const user = await this.usersServices.findOneByEmail(email);
@@ -14,8 +20,16 @@ export class AuthService {
         const { password, ...result } = user;
         return result;
       }
-      return null;
     }
     return null;
+  }
+
+  generateJWT(user: User) {
+    // TODO: el sub es un idetificador unico del usuario
+    const payload: Token = { role: user.role, sub: user.id };
+    return {
+      access_token: this.jwtServices.sign(payload),
+      user,
+    };
   }
 }
